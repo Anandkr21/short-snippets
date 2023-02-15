084854b6e62df0f7361326e902b1e0a36192ce72
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-02-16 03:29:54 +0530

    initial commit

--- Authentication/index.js
+++ Authentication/index.js
@@ -1,47 +1,76 @@
-const express=require('express');
-const {connection}=require('./config/db')
-const {userModel}=require('./model/user.model')
-
-const app=express()
+const express = require('express');
+const { connection } = require('./config/db')
+const { userModel } = require('./model/user.model')
+const jwt = require('jsonwebtoken')
+const app = express()
 
 app.use(express.json())
 
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.send('HOME PAGE')
 })
 
-app.post('/register', async(req,res) =>{
-    const userDetail=req.body
-    try{
+app.post('/register', async (req, res) => {
+    const userDetail = req.body
+    try {
         const user = new userModel(userDetail)
         await user.save()
-        res.send({'msg':'User Registered'})
-    }catch(err){
-        res.send({'msg':'Something went wrong', 'eroor':err.message})
+        res.send({ 'msg': 'User Registered' })
+    } catch (err) {
+        res.send({ 'msg': 'Something went wrong', 'eroor': err.message })
     }
-   
+
 })
 
-app.post('/login', async(req,res) =>{
-    const {email,pass}=req.body
-    try{
-        const user=await userModel.find({email,pass})
-        if(user.length>0){
-            res.send({'msg':'Login Successful'})
-        }else{
-            res.send({'msg':'Wrong credentials.'})
+app.post('/login', async (req, res) => {
+    const token = jwt.sign({ course: 'backend' }, 'masai');
+    const { email, pass } = req.body
+    try {
+        const user = await userModel.find({ email, pass })
+        if (user.length > 0) {
+            res.send({ 'msg': 'Login Successful', 'token': token })
+        } else {
+            res.send({ 'msg': 'Wrong credentials.' })
         }
-    }catch(err){ 
-            res.send({"msg":"Something went wrong!"})
+    } catch (err) {
+        res.send({ "msg": "Something went wrong!", 'error': err.message })
     }
 })
 
 
-app.listen(8080, async()=>{
-    try{
+app.get('/data', (req, res) => {
+    const token = req.headers.authorization
+    // verify a token symmetric
+    jwt.verify(token, 'masai', (err, decoded) => {
+        if (decoded) {
+            res.send({ 'msg': 'data is here...' })
+        } else {
+            res.send({ "msg": "Something went wrong!", 'error': err.message })
+        }
+    });
+})
+
+
+app.get('/cart', (req, res) => {
+    const token = req.headers.authorization
+    jwt.verify(token, 'masai', (err, decoded) => {
+        if (decoded) {
+            res.send({ 'msg': 'cart data is here...' })
+        } else {
+            res.send({ "msg": "Something went wrong!", 'error': err.message })
+        }
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.send('about page')
+})
+
+app.listen(8080, async () => {
+    try {
         await connection
         console.log("Connectedto DB");
-    }catch(err){
+    } catch (err) {
         console.log(err);
     }
     console.log('Server is running at 8080');
